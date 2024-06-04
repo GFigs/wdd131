4516bdb2879ad3496869de8bd64e8fed951e9a4c
GFigs <sgabrielfigueroa95@gmail.com>
2024-06-04 20:47:04 +0200

    Week 5 activity

---
+++ scripts/review.js
@@ -0,0 +1,9 @@
+let reviewCount = localStorage.getItem("reviewCount");
+if (!reviewCount) {
+    reviewCount = 0;
+}
+reviewCount++;
+
+localStorage.setItem("reviewCount", reviewCount);
+
+document.getElementById("reviewCount").textContent = reviewCount;
